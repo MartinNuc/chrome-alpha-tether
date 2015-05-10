8e6fb9dbb707f697f84eadc42b2331ccac4b07a6
Martin Nuc <martin@nuc.cz>
2015-05-10 17:47:28 +0200

    App starts in window. Still have to figure out the inner box size.

--- scripts/background.js
+++ scripts/background.js
@@ -1,12 +1,10 @@
 chrome.app.runtime.onLaunched.addListener(function(launchData) {
-    chrome.app.window.create('../index.html', {
-        id: "AlphaTether",
-        bounds: {
-            width: 1200,
-            height: 800
-        },
-        minWidth: 1200,
-        minHeight: 800,
-        frame: 'none'
-    });
+    var optionsDictionary = {id: "AlphaTether"};
+    optionsDictionary.innerBounds = {};
+    optionsDictionary.innerBounds.left = 0;
+    optionsDictionary.innerBounds.top = 0;
+    optionsDictionary.innerBounds.width = screen.width;
+    optionsDictionary.innerBounds.height = screen.height;
+
+    chrome.app.window.create('../index.html', optionsDictionary);
 });

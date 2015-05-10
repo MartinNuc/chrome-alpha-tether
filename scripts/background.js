9a2dbc3320d36b9591eedf0c352d959e893c2352
Martin Nuc <martin@nuc.cz>
2015-05-10 16:13:21 +0200

    Changes window size to 1200x800.

--- scripts/background.js
+++ scripts/background.js
@@ -2,11 +2,11 @@ chrome.app.runtime.onLaunched.addListener(function(launchData) {
     chrome.app.window.create('../index.html', {
         id: "AlphaTether",
         bounds: {
-            width: 800,
-            height: 600
+            width: 1200,
+            height: 800
         },
-        minWidth: 800,
-        minHeight: 600,
+        minWidth: 1200,
+        minHeight: 800,
         frame: 'none'
     });
 });
